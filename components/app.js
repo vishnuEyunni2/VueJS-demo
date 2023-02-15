b5a3e7a82684a70e5722d74a54d46a47a492710c
vishnuEyunni2 <vishnuvardhan.eyunni@gmail.com>
2023-02-15 23:05:01 +0530

    separated JS and template

---
+++ components/app.js
@@ -0,0 +1,10 @@
+Vue.createApp({})
+  .component("click-counter", {
+    template: '#click-counter-template',
+    data() {
+      return {
+        count: 0,
+      };
+    },
+  })
+  .mount("#app");
